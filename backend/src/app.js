124534daccb9a5a9838550d16ee64385915b7d85
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-01-09 11:55:58 +0530

    playing with routing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Good Morning Nuruz");
-});
+// app.use("/",(req,res)=>{
+//     res.send("Good Morning Nuruz");
+// });
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Nuruz");
-});
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello Nuruz");
+// });
 
-app.use("/test",(req,res)=>{
-    res.send("Hello Hello from the server");
+// app.use("/test",(req,res)=>{
+//     res.send("Hello Hello from the server");
+// });
+app.length("/user/:userId/:name/:password", (req,res)=> {
+    console.log(req.params);
+    res.send({firstName: "Nuruz", lastName: "Zaman"})
 });
-
 app.listen(3000,()=> {
     console.log("Server is listening on port 3000....");
 })
