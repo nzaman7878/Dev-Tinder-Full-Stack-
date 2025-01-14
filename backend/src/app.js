03e690d05d8774714e985dc342ff0ff3594b871e
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-01-14 23:46:13 +0530

    get api

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,11 +17,45 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
     app.listen(3000, () => {
-      console.log("Server is successfully listening on port 3000...");
+      console.log("Server is successfully listening on port 7777...");
     });
   })
   .catch((err) => {
