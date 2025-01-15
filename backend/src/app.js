ebf698785b01d812d28543c6867f4dbdbd3a09ce
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-01-15 19:56:30 +0530

    added the update api user

--- backend/src/app.js
+++ backend/src/app.js
@@ -64,11 +64,26 @@ app.delete("/user", async (req, res) => {
   }
 });
 
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
     app.listen(3000, () => {
-      console.log("Server is successfully listening on port 7777...");
+      console.log("Server is successfully listening on port 3000...");
     });
   })
   .catch((err) => {
