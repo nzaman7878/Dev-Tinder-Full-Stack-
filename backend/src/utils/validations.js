58bb8d55eeae327b378e488b3def5ac43e64d4d3
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-01-26 01:12:14 +0530

    added logout + profile edit

--- backend/src/utils/validations.js
+++ backend/src/utils/validations.js
@@ -1,4 +1,5 @@
 const validator = require("validator");
+
 const validateSignUpData = (req) => {
   const { firstName, lastName, emailId, password } = req.body;
   if (!firstName || !lastName) {
@@ -9,6 +10,27 @@ const validateSignUpData = (req) => {
     throw new Error("Please enter a strong Password!");
   }
 };
+
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
 module.exports = {
   validateSignUpData,
+  validateEditProfileData,
 };
