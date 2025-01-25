c08db058620370b5e53eba720f301d63fa5d947e
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-01-25 23:50:39 +0530

    auth middlewares + schema methods

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,25 +1,29 @@
-const adminAuth = (req, res, next)=>{
-    console.log("Admin auth is getting checked");
-    const token = "xyz";
-    const isAdminAuthorized = token ==="xyz";
-    if (!isAdminAuthorized){
-        res.status(401).send("unauthorized request");
-    } else {
-        next();
-    }
-};
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const userAuth = (req, res, next)=>{
-    console.log("user auth is getting checked");
-    const token = "xyz";
-    const isAdminAuthorized = token ==="xyz";
-    if (!isAdminAuthorized){
-        res.status(401).send("unauthorized request");
-    } else {
-        next();
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token is not valid!!!!!!!!!");
     }
+
+    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
 };
 
 module.exports = {
-    adminAuth,userAuth
+  userAuth,
 };
