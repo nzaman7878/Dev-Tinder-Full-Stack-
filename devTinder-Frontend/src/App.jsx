dfb4fa0eeaa9b461e64c56c45c91c18388a4a266
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-02-03 00:40:46 +0530

    set up the routing + footer cmp

--- devTinder-Frontend/src/App.jsx
+++ devTinder-Frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-import NavBar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import "./App.css"
+
 
 function App() {
-  
-
   return (
     <>
-     <NavBar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
