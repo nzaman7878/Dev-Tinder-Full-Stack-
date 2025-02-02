0fd53ab1433b4b89ef1117cba8dd66e0e8b38baf
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-02-02 22:21:25 +0530

    added navbar commponent to app.jsx

--- devTinder-Frontend/src/App.jsx
+++ devTinder-Frontend/src/App.jsx
@@ -5,15 +5,13 @@ function App() {
 
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-200">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">😍DevTinder</a>
   </div>
   <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div className="dropdown dropdown-end">
+    
+    <div className="dropdown dropdown-end mx-5" >
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
