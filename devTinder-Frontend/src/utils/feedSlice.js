eb425e93480e56eb56ebbffa5bbd62c63a92cacd
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-02-20 23:25:23 +0530

    Send/Ignore the user card from the feed

--- devTinder-Frontend/src/utils/feedSlice.js
+++ devTinder-Frontend/src/utils/feedSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const feedSlice = createSlice({
   name: "feed",
   initialState: null,
@@ -6,10 +7,12 @@ const feedSlice = createSlice({
     addFeed: (state, action) => {
       return action.payload;
     },
-    removeFeed: (state, action) => {
-      return null;
+    removeUserFromFeed: (state, action) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);
+      return newFeed;
     },
   },
 });
-export const { addFeed } = feedSlice.actions;
+
+export const { addFeed, removeUserFromFeed } = feedSlice.actions;
 export default feedSlice.reducer;
