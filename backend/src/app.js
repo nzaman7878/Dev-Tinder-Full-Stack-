9f93e044578bf6aca4085888f87c0b10b5ed7f52
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-07-08 13:52:12 +0530

    feat: integrate Razorpay for payment processing and add membership features

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+require("./utils/cronjob");
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -19,11 +21,13 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const paymentRouter = require("./routes/payment");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", paymentRouter);
 
 connectDB()
   .then(() => {
